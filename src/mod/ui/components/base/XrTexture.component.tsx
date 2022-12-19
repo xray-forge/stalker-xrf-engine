bcc73170f3e284646d14d3bda137b10588328d5d
Neloreck <Neloreck@gmail.com>
2022-12-19 16:05:20 +0200

    XR texture with different states.

--- src/mod/ui/components/base/XrTexture.component.tsx
+++ src/mod/ui/components/base/XrTexture.component.tsx
@@ -3,12 +3,23 @@ import { JSXXML, JSXNode } from "jsx-xml";
 import { TTextureId } from "@/mod/globals/textures";
 
 interface IXrTextureProps {
-  id: TTextureId;
+  id?: TTextureId;
+  idEnabled?: TTextureId;
+  idTouched?: TTextureId;
+  idDisabled?: TTextureId;
+  idHighlighted: TTextureId;
 }
 
 /**
- * Texture description.
+ * Render texture descriptors for an XML node.
+ * Requires parameters to control state-id matching.
  */
-export function XrTexture({ id }: IXrTextureProps): JSXNode {
-  return <texture>{id}</texture>;
+export function XrTexture({ id, idHighlighted, idTouched, idDisabled, idEnabled }: IXrTextureProps): JSXNode {
+  return [
+    id ? <texture>{id}</texture> : null,
+    idEnabled ? <texture> {idEnabled} </texture> : null,
+    idTouched ? <texture> {idTouched} </texture> : null,
+    idDisabled ? <texture> {idDisabled} </texture> : null,
+    idHighlighted ? <texture> {idHighlighted} </texture> : null
+  ];
 }
