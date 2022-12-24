3700e05b6b7e2090ee667f6133eaf54965c54d78
Neloreck <Neloreck@gmail.com>
2022-12-25 01:47:30 +0200

    Separated table utils.

--- src/mod/scripts/utils/general.ts
+++ src/mod/scripts/utils/general.ts
@@ -1,37 +1,7 @@
-import { AnyObject, Optional } from "@/mod/lib/types";
+import { Optional } from "@/mod/lib/types";
 import { LuaLogger } from "@/mod/scripts/utils/logging";
 
 const log: LuaLogger = new LuaLogger("utils/general");
-
-/**
- * Check if provided container is empty collection.
- * Very lua-specific checks, do not apply TS logic here.
- */
-export function isEmpty(container: Optional<LuaIterable<any>>): boolean {
-  if (container === null) {
-    return true;
-  }
-
-  if (type(container) === "function") {
-    for (const it of container) {
-      return false;
-    }
-
-    return true;
-  }
-
-  assert(type(container) == "table");
-
-  if ((container as AnyObject)[1] !== null) {
-    return false;
-  }
-
-  for (const [k, v] of pairs(container)) {
-    return false;
-  }
-
-  return true;
-}
 
 /**
  * todo: description
@@ -76,15 +46,6 @@ export function randomNumber(min?: number, max?: number): number {
     return math.random();
   } else {
     return math.random(min, max);
-  }
-}
-
-/**
- * todo: description
- */
-export function clearTable(tbl: LuaTable): void {
-  while (tbl.length() !== 0) {
-    table.remove(tbl, tbl.length());
   }
 }
 
