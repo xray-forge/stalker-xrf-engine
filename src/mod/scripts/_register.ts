c27c8e6ffc6645681c14f56e57a6b9d56bb9ed3e
Neloreck <Neloreck@gmail.com>
2023-02-05 19:01:59 +0200

    Clean-only script. Renaming log -> logger for _register.

--- src/mod/scripts/_register.ts
+++ src/mod/scripts/_register.ts
@@ -1,40 +1,38 @@
-/* @noSelfInFile */
 /* eslint prefer-const: 0,  @typescript-eslint/no-var-requires: 0 */
 
 import { XR_object_factory } from "xray16";
 
 import { LuaLogger } from "@/mod/scripts/utils/logging";
 
-declare let register_classes: (object_factory: XR_object_factory) => void;
-declare let get_game_clsid: (gameTypeOption: string, isServer: boolean) => void;
-declare let get_ui_clsid: (gameType: string) => void;
-
-const log: LuaLogger = new LuaLogger("register");
+const logger: LuaLogger = new LuaLogger("register");
 
 /**
  * Registration:
  */
+declare let register_classes: (object_factory: XR_object_factory) => void;
+declare let get_game_clsid: (gameTypeOption: string, isServer: boolean) => void;
+declare let get_ui_clsid: (gameType: string) => void;
 
 register_classes = (object_factory) => {
-  log.info("Register classes:");
+  logger.info("Register classes:");
 
   const { registerGameClasses } = require("@/mod/scripts/registering/class_registrator");
 
   registerGameClasses(object_factory);
 };
 
-get_game_clsid = (...args) => {
-  log.info("Register clsid:");
+get_game_clsid = (gameTypeOption: string, isServer: boolean) => {
+  logger.info("Register clsid:");
 
   const { getGameClsId } = require("@/mod/scripts/registering/game_registrator");
 
-  getGameClsId(...args);
+  getGameClsId(gameTypeOption, isServer);
 };
 
-get_ui_clsid = (...args) => {
-  log.info("Register ui clsid:");
+get_ui_clsid = (gameType: string) => {
+  logger.info("Register ui clsid:");
 
   const { getUiClsId } = require("@/mod/scripts/registering/ui_registrator");
 
-  getUiClsId(...args);
+  getUiClsId(gameType);
 };
