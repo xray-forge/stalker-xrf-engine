62927e2f167c2ea61ffbd182e256a1d53ddb8267
Neloreck <Neloreck@gmail.com>
2023-03-05 16:56:17 +0200

    Sceheme oscillate improved.

--- src/mod/scripts/core/schemes/ph_oscillate/SchemeOscillate.ts
+++ src/mod/scripts/core/schemes/ph_oscillate/SchemeOscillate.ts
@@ -1,14 +1,14 @@
-import { device, time_global, vector, XR_game_object, XR_ini_file, XR_physics_joint, XR_vector } from "xray16";
+import { XR_game_object, XR_ini_file } from "xray16";
 
-import { EScheme, ESchemeType, Optional, TSection } from "@/mod/lib/types";
-import { IStoredObject } from "@/mod/scripts/core/database";
+import { EScheme, ESchemeType, TName, TSection } from "@/mod/lib/types";
 import { assignStorageAndBind } from "@/mod/scripts/core/schemes/assignStorageAndBind";
 import { AbstractScheme } from "@/mod/scripts/core/schemes/base/AbstractScheme";
+import { ISchemeOscillateState } from "@/mod/scripts/core/schemes/ph_oscillate/ISchemeOscillateState";
+import { OscillateManager } from "@/mod/scripts/core/schemes/ph_oscillate/OscillateManager";
 import { subscribeActionForEvents } from "@/mod/scripts/core/schemes/subscribeActionForEvents";
 import { getConfigNumber, getConfigString, getConfigSwitchConditions } from "@/mod/scripts/utils/configs";
 import { abort } from "@/mod/scripts/utils/debug";
 import { LuaLogger } from "@/mod/scripts/utils/logging";
-import { vectorRotateY } from "@/mod/scripts/utils/physics";
 
 const logger: LuaLogger = new LuaLogger("SchemeOscillate");
 
@@ -19,28 +19,34 @@ export class SchemeOscillate extends AbstractScheme {
   public static override readonly SCHEME_SECTION: EScheme = EScheme.PH_OSCILLATE;
   public static override readonly SCHEME_TYPE: ESchemeType = ESchemeType.ITEM;
 
+  /**
+   * todo;
+   */
   public static override addToBinder(
     object: XR_game_object,
     ini: XR_ini_file,
     scheme: EScheme,
     section: TSection,
-    state: IStoredObject
+    state: ISchemeOscillateState
   ): void {
     logger.info("Add to binder:", object.name());
-    subscribeActionForEvents(object, state, new SchemeOscillate(object, state));
+    subscribeActionForEvents(object, state, new OscillateManager(object, state));
   }
 
+  /**
+   * todo;
+   */
   public static override setScheme(
     object: XR_game_object,
     ini: XR_ini_file,
     scheme: EScheme,
     section: TSection,
-    gulag_name: string
+    gulagName: TName
   ): void {
-    const state: IStoredObject = assignStorageAndBind(object, ini, scheme, section);
+    const state: ISchemeOscillateState = assignStorageAndBind(object, ini, scheme, section);
 
     state.logic = getConfigSwitchConditions(ini, section, object);
-    state.joint = getConfigString(ini, section, "joint", object, true, gulag_name);
+    state.joint = getConfigString(ini, section, "joint", object, true, gulagName);
 
     if (state.joint === null) {
       abort("Invalid joint definition for object %s", object.name());
@@ -49,56 +55,16 @@ export class SchemeOscillate extends AbstractScheme {
     state.period = getConfigNumber(ini, section, "period", object, true, 0);
     state.force = getConfigNumber(ini, section, "force", object, true, 0);
 
+    // todo: is real with 0s as default values?
     if (state.period === null || state.force === null) {
       abort("[ActionOscillate] Error : Force or period not defined");
     }
 
     state.angle = getConfigNumber(ini, section, "correct_angle", object, false, 0);
 
+    // todo: is real with 0s as default values?
     if (state.angle === null) {
       state.angle = 0;
     }
-  }
-
-  public time: number = 0;
-  public coefficient: number = 0;
-  public dir: XR_vector = new vector().set(math.random(), 0, math.random()).normalize();
-  public joint: Optional<XR_physics_joint> = null;
-  public pause: boolean = false;
-
-  public override resetScheme(): void {
-    this.time = device().time_global();
-    this.dir = new vector().set(math.random(), 0, math.random()).normalize();
-    this.coefficient = this.state.force / this.state.period;
-    this.joint = this.object.get_physics_shell()!.get_joint_by_bone_name(this.state.joint);
-    this.time = time_global();
-    this.pause = false;
-  }
-
-  public override update(delta: number): void {
-    const c_time = time_global();
-
-    if (this.pause === true) {
-      if (c_time - this.time < this.state.period * 0.5) {
-        return;
-      }
-
-      this.time = c_time;
-      this.pause = false;
-    }
-
-    if (c_time - this.time >= this.state.period) {
-      this.dir.x = -this.dir.x;
-      this.dir.z = -this.dir.z;
-      this.dir = vectorRotateY(new vector().set(-this.dir.x, 0, -this.dir.z), this.state.angle);
-      this.time = c_time;
-      this.pause = true;
-
-      return;
-    }
-
-    const force = (c_time - this.time) * this.coefficient;
-
-    this.object.set_const_force(this.dir, force, 2);
   }
 }
---
+++ src/mod/scripts/core/schemes/ph_oscillate/index.ts
@@ -0,0 +1,2 @@
+export * from "@/mod/scripts/core/schemes/ph_oscillate/SchemeOscillate";
+export * from "@/mod/scripts/core/schemes/ph_oscillate/ISchemeOscillateState";
