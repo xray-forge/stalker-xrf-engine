882afe14719e909dc29009d9cbdf05fa5bdb26f1
Neloreck <Neloreck@gmail.com>
2023-05-18 03:01:33 +0300

    Extend 'save_markers' module docs. Add tests for markers utils.

---
+++ src/engine/core/database/save_markers.test.ts
@@ -0,0 +1,61 @@
+import { describe, expect, it, jest } from "@jest/globals";
+
+import { registry } from "@/engine/core/database/registry";
+import { closeLoadMarker, closeSaveMarker, openLoadMarker, openSaveMarker } from "@/engine/core/database/save_markers";
+import { MAX_I32 } from "@/engine/lib/constants/memory";
+import { EPacketDataType, mockNetPacket, MockNetProcessor } from "@/fixtures/xray";
+
+describe("'save_markers' databse module", () => {
+  it("should correctly create save markers", () => {
+    const netProcessor: MockNetProcessor = new MockNetProcessor();
+
+    openSaveMarker(mockNetPacket(netProcessor), "test-1");
+
+    expect(registry.saveMarkers.get("test-1")).toBe(0);
+
+    jest.spyOn(netProcessor, "w_tell").mockImplementation(() => 100);
+    openSaveMarker(mockNetPacket(netProcessor), "test-2");
+    expect(registry.saveMarkers.get("test-2")).toBe(100);
+
+    jest.spyOn(netProcessor, "w_tell").mockImplementation(() => MAX_I32);
+    expect(() => openSaveMarker(mockNetPacket(netProcessor), "test-3")).toThrow();
+
+    jest.spyOn(netProcessor, "w_tell").mockImplementation(() => 10);
+    expect(closeSaveMarker(mockNetPacket(netProcessor), "test-1")).toBe(10);
+    expect(netProcessor.writeDataOrder).toEqual([EPacketDataType.U16]);
+    expect(netProcessor.dataList).toEqual([10]);
+
+    jest.spyOn(netProcessor, "w_tell").mockImplementation(() => 150);
+    expect(closeSaveMarker(mockNetPacket(netProcessor), "test-2")).toBe(50);
+    expect(netProcessor.writeDataOrder).toEqual([EPacketDataType.U16, EPacketDataType.U16]);
+    expect(netProcessor.dataList).toEqual([10, 50]);
+
+    expect(() => closeSaveMarker(mockNetPacket(netProcessor), "test-3")).toThrow();
+  });
+
+  it("should correctly create load markers", () => {
+    const netProcessor: MockNetProcessor = new MockNetProcessor();
+
+    netProcessor.w_u16(10);
+    netProcessor.w_u16(50);
+
+    expect(netProcessor.dataList).toEqual([10, 50]);
+
+    openLoadMarker(mockNetPacket(netProcessor), "test-4");
+    expect(registry.saveMarkers.get("test-4")).toBe(0);
+
+    jest.spyOn(netProcessor, "r_tell").mockImplementation(() => 100);
+    openLoadMarker(mockNetPacket(netProcessor), "test-5");
+    expect(registry.saveMarkers.get("test-5")).toBe(100);
+
+    jest.spyOn(netProcessor, "r_tell").mockImplementation(() => 10);
+    expect(closeLoadMarker(mockNetPacket(netProcessor), "test-4")).toBe(10);
+    expect(netProcessor.readDataOrder).toEqual([EPacketDataType.U16]);
+    expect(netProcessor.dataList).toEqual([50]);
+
+    jest.spyOn(netProcessor, "r_tell").mockImplementation(() => 150);
+    expect(closeLoadMarker(mockNetPacket(netProcessor), "test-5")).toBe(50);
+    expect(netProcessor.readDataOrder).toEqual([EPacketDataType.U16, EPacketDataType.U16]);
+    expect(netProcessor.dataList).toEqual([]);
+  });
+});
--- src/engine/core/database/save_markers.ts
+++ src/engine/core/database/save_markers.ts
@@ -8,43 +8,24 @@ import { TCount, TName } from "@/engine/lib/types";
 const logger: LuaLogger = new LuaLogger($filename);
 
 /**
- * todo
- */
-export function openLoadMarker(reader: TXR_net_processor, markerName: TName): void {
-  registry.saveMarkers.set(markerName, reader.r_tell());
-}
-
-/**
- * todo
- */
-export function closeLoadMarker(reader: TXR_net_processor, markerName: TName): void {
-  assert(registry.saveMarkers.get(markerName) !== null, "Trying to check without marker: '%s'", markerName);
-
-  const actualDiff: TCount = reader.r_tell() - registry.saveMarkers.get(markerName);
-  const expectedDiff: TCount = reader.r_u16();
-
-  assert(
-    expectedDiff === actualDiff,
-    "Incorrect load '%s': <dif: %s>, <cdif: %s>.",
-    markerName,
-    expectedDiff,
-    actualDiff
-  );
-}
-
-/**
- * todo
+ * Open save marker and verify net packet packet size.
+ *
+ * @param packet - net packet to save data in
+ * @param markerName - net packet transaction marker to verify data integrity
  */
 export function openSaveMarker(packet: XR_net_packet, markerName: TName): void {
-  registry.saveMarkers.set(markerName, packet.w_tell());
-
   assert(packet.w_tell() < 16_000, "You are saving too much in '%s'.", markerName);
+  registry.saveMarkers.set(markerName, packet.w_tell());
 }
 
 /**
- * todo
+ * Close save marker and verify net packet packet size.
+ *
+ * @param packet - net packet to save data in
+ * @param markerName - net packet transaction marker to verify data integrity
+ * @returns marker transaction saving size
  */
-export function closeSaveMarker(packet: XR_net_packet, markerName: TName): void {
+export function closeSaveMarker(packet: XR_net_packet, markerName: TName): TCount {
   assert(registry.saveMarkers.get(markerName) !== null, "Trying to check without marker: '%s'.", markerName);
 
   const markerDif: TCount = packet.w_tell() - registry.saveMarkers.get(markerName);
@@ -58,4 +39,40 @@ export function closeSaveMarker(packet: XR_net_packet, markerName: TName): void 
   }
 
   packet.w_u16(markerDif);
+
+  return markerDif;
+}
+
+/**
+ * Open load marker.
+ *
+ * @param reader - reader to load data from
+ * @param markerName - reader transaction marker to verify data integrity
+ */
+export function openLoadMarker(reader: TXR_net_processor, markerName: TName): void {
+  registry.saveMarkers.set(markerName, reader.r_tell());
+}
+
+/**
+ * Close load marker and verify net reader size.
+ *
+ * @param reader - reader to load data from
+ * @param markerName - reader transaction marker to verify data integrity
+ * @returns marker transaction loading size
+ */
+export function closeLoadMarker(reader: TXR_net_processor, markerName: TName): TCount {
+  assert(registry.saveMarkers.get(markerName) !== null, "Trying to check without marker: '%s'", markerName);
+
+  const actualDiff: TCount = reader.r_tell() - registry.saveMarkers.get(markerName);
+  const expectedDiff: TCount = reader.r_u16();
+
+  assert(
+    expectedDiff === actualDiff,
+    "Incorrect load '%s': <dif: %s>, <cdif: %s>.",
+    markerName,
+    expectedDiff,
+    actualDiff
+  );
+
+  return actualDiff;
 }
