9a1d354ec4843a8ca2d13544a290e790ee8884ea
Neloreck <Neloreck@gmail.com>
2023-06-10 22:39:22 +0300

    Verify that suggested path exists in OS for get_game_paths.

--- cli/utils/fs/get_game_paths.ts
+++ cli/utils/fs/get_game_paths.ts
@@ -3,9 +3,7 @@ import * as path from "path";
 import { getAppPath } from "steam-path";
 
 import { default as config } from "#/config.json";
-
-const STEAM_GAME_ID: number = config.targets.stalker_game_steam_id;
-const FALLBACK_GAME_PATH: string = config.targets.stalker_game_fallback_path;
+import { exists } from "#/utils";
 
 const GAME_PATHS = {
   root: "",
@@ -23,19 +21,19 @@ type TGamePaths = typeof GAME_PATHS;
  * Get absolute paths to the game assets/executables/directories.
  */
 export async function getGamePaths(): Promise<TGamePaths> {
-  const { path: gamePath } = await getAppPath(STEAM_GAME_ID).catch(() => {
-    if (FALLBACK_GAME_PATH) {
-      return { path: FALLBACK_GAME_PATH };
-    }
-
-    throw new Error("Steam not found and no fallback game path provided.");
+  const { path: gamePath } = await getAppPath(config.targets.stalker_game_steam_id).catch(() => {
+    return { path: config.targets.stalker_game_fallback_path };
   });
+
+  if (!(await exists(gamePath))) {
+    throw new Error(`Suggested game path does not exist: '${gamePath}'.`);
+  }
 
   return Object.entries(GAME_PATHS).reduce((paths, [name, location]) => {
     const normalPath: string = path.normalize(location);
-    const isAbs: boolean = path.isAbsolute(normalPath);
+    const isAbsolute: boolean = path.isAbsolute(normalPath);
 
-    paths[name] = isAbs ? normalPath : path.join(gamePath, normalPath);
+    paths[name] = isAbsolute ? normalPath : path.join(gamePath, normalPath);
 
     return paths;
   }, {} as TGamePaths);
