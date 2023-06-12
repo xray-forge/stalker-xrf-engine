6363f9d47e431f4d0eae65b6326f4e882883c5bd
Neloreck <Neloreck@gmail.com>
2023-06-12 22:36:46 +0300

    Add definitions for global lua libs from open-xray.

---
+++ src/typedefs/extensions/index.d.ts
@@ -0,0 +1,55 @@
+/**
+ * Extension methods for table.
+ */
+declare namespace table {
+  /**
+   * Get random value from table.
+   * Provided by lua extensions lib in open-xray.
+   *
+   * @returns random value from table
+   */
+  function random<K extends AnyNotNil, V>(list: LuaTable<K, V>): V;
+  /**
+   * Get table size.
+   * Provided by lua extensions lib in open-xray.
+   *
+   * @returns table size
+   */
+  function size<K extends AnyNotNil, V>(list: LuaTable<K, V>): number;
+  /**
+   * Get table keys.
+   * Provided by lua extensions lib in open-xray.
+   *
+   * @returns list of keys in an unordered way
+   */
+  function keys<K extends AnyNotNil, V>(list: LuaTable<K, V>): LuaTable<number, K>;
+  /**
+   * Get table values.
+   * Provided by lua extensions lib in open-xray.
+   *
+   * @returns list of values in an unordered way
+   */
+  function values<K extends AnyNotNil, V>(list: LuaTable<K, V>): LuaTable<number, V>;
+}
+
+/**
+ * Extension methods for string.
+ */
+declare namespace string {
+  /**
+   * Trim all spaces from both sides of string.
+   */
+  function trim(value: string): string;
+  /**
+   * Trim spaces from left side of string.
+   */
+  function trim_l(value: string): string;
+  /**
+   * Trim spaces from right side of string.
+   */
+  function trim_r(value: string): string;
+  /**
+   * Trim everything separated by spaces from first text entry.
+   */
+  function trim_w(value: string): string;
+}
---
+++ src/typedefs/mashal/index.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Marshal LUA library.
+ * https://github.com/richardhundt/lua-marshal
+ */
+declare namespace marshal {
+  /**
+   * Deep clone a value (deep for tables and functions).
+   */
+  function clone<T extends LuaTable | Record<any, any>>(table: T): T;
+  /**
+   * Deserializes a byte stream to a value.
+   */
+  function decode<T extends LuaTable | Record<any, any>>(serialized: string, constants?: Partial<T>): T;
+  /**
+   * Serialize a value to a byte stream.
+   */
+  function encode<T extends LuaTable | Record<any, any>>(table: T, constants?: Partial<T>): string;
+}
