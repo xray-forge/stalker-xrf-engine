2a29e55d843fe7f2068f02cc65133c3d7c16f2f0
Neloreck <Neloreck@gmail.com>
2023-06-12 03:41:18 +0300

    Reformat spawn sections ltx.

---
+++ src/engine/configs/creatures/spawn_sections.ts
@@ -0,0 +1,12 @@
+import { LTX_INCLUDE } from "#/utils";
+
+export const config = {
+  [LTX_INCLUDE]: [
+    "spawn_sections_general.ltx",
+    "spawn_sections_zaton.ltx",
+    "spawn_sections_pripyat.ltx",
+    "spawn_sections_jupiter.ltx",
+    "spawn_sections_labx8.ltx",
+    "spawn_sections_underpass.ltx",
+  ],
+};
