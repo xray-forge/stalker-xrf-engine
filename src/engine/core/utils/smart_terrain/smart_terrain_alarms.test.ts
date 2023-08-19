3039527c3aed133bb012c48bcf61da8e2fc225ac
Neloreck <Neloreck@gmail.com>
2023-08-19 04:16:30 +0300

    Smart terrain alarm utils test added.

---
+++ src/engine/core/utils/smart_terrain/smart_terrain_alarms.test.ts
@@ -0,0 +1,50 @@
+import { describe, expect, it, jest } from "@jest/globals";
+import { CTime, game } from "xray16";
+
+import { SmartTerrain } from "@/engine/core/objects";
+import {
+  startSmartTerrainAlarm,
+  updateSmartTerrainAlarmStatus,
+} from "@/engine/core/utils/smart_terrain/smart_terrain_alarms";
+import { mockSmartTerrain } from "@/fixtures/engine";
+import { MockCTime } from "@/fixtures/xray";
+
+describe("smart_terrain_alarms utils", () => {
+  it("'startSmartTerrainAlarm' should correctly start alarm", () => {
+    const smartTerrain: SmartTerrain = mockSmartTerrain();
+    const nowTime: CTime = MockCTime.mock(2015, 6, 14, 13, 25, 30, 500);
+    const laterTime: CTime = MockCTime.mock(2015, 6, 15, 13, 25, 30, 500);
+
+    expect(smartTerrain.alarmStartedAt).toBeNull();
+
+    jest.spyOn(game, "get_game_time").mockImplementation(() => nowTime);
+    startSmartTerrainAlarm(smartTerrain);
+    expect(smartTerrain.alarmStartedAt).toBe(nowTime);
+
+    jest.spyOn(game, "get_game_time").mockImplementation(() => laterTime);
+    startSmartTerrainAlarm(smartTerrain);
+    expect(smartTerrain.alarmStartedAt).toBe(laterTime);
+  });
+
+  it("'updateSmartTerrainAlarmStatus' should correctly handle alarm", () => {
+    const smartTerrain: SmartTerrain = mockSmartTerrain();
+    const startTime: CTime = MockCTime.mock(2015, 6, 14, 12, 25, 30, 500);
+    const laterTime: CTime = MockCTime.mock(2015, 6, 14, 15, 25, 30, 500);
+    const endedTime: CTime = MockCTime.mock(2015, 6, 14, 18, 25, 30, 500); // Exactly 6 hours.
+
+    expect(smartTerrain.alarmStartedAt).toBeNull();
+
+    jest.spyOn(game, "get_game_time").mockImplementation(() => startTime);
+    startSmartTerrainAlarm(smartTerrain);
+    updateSmartTerrainAlarmStatus(smartTerrain);
+    expect(smartTerrain.alarmStartedAt).toBe(startTime);
+
+    jest.spyOn(game, "get_game_time").mockImplementation(() => laterTime);
+    updateSmartTerrainAlarmStatus(smartTerrain);
+    expect(smartTerrain.alarmStartedAt).toBe(startTime);
+
+    jest.spyOn(game, "get_game_time").mockImplementation(() => endedTime);
+    updateSmartTerrainAlarmStatus(smartTerrain);
+    expect(smartTerrain.alarmStartedAt).toBeNull();
+  });
+});
--- src/engine/core/utils/smart_terrain/smart_terrain_alarms.ts
+++ src/engine/core/utils/smart_terrain/smart_terrain_alarms.ts
@@ -4,19 +4,23 @@ import { SmartTerrain } from "@/engine/core/objects";
 import { logicsConfig } from "@/engine/lib/configs/LogicsConfig";
 
 /**
- * todo;
+ * Start alarm for smart terrain.
+ *
+ * @param smartTerrain - smart terrain to start alarm for
  */
 export function startSmartTerrainAlarm(smartTerrain: SmartTerrain): void {
   smartTerrain.alarmStartedAt = game.get_game_time();
 }
 
 /**
- * todo;
+ * Update status of smart terrain alarm, check whether it should be turned off.
+ *
+ * @param smartTerrain - smart terrain to try updating for
  */
 export function updateSmartTerrainAlarmStatus(smartTerrain: SmartTerrain): void {
   if (
     smartTerrain.alarmStartedAt !== null &&
-    game.get_game_time().diffSec(smartTerrain.alarmStartedAt) > logicsConfig.SMART_TERRAIN.ALARM_SMART_TERRAIN_GENERIC
+    game.get_game_time().diffSec(smartTerrain.alarmStartedAt) >= logicsConfig.SMART_TERRAIN.ALARM_SMART_TERRAIN_GENERIC
   ) {
     smartTerrain.alarmStartedAt = null;
   }
