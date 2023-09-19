1d3203b348d5832028f4cd4d407ca1b547757a61
Neloreck <Neloreck@gmail.com>
2023-09-19 04:08:54 +0300

    Verify start callback calls.

--- src/engine/scripts/start.test.ts
+++ src/engine/scripts/start.test.ts
@@ -1,6 +1,17 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
 
+jest.mock("@/engine/core/utils/class_ids", () => ({ updateClassIds: jest.fn(), createClassIds: jest.fn() }));
+jest.mock("@/engine/core/utils/ini/ini_system", () => ({ unlockSystemIniOverriding: jest.fn() }));
+jest.mock("@/engine/scripts/register/extensions_registrator", () => ({ registerExtensions: jest.fn() }));
+jest.mock("@/engine/scripts/register/managers_registrator", () => ({ registerManagers: jest.fn() }));
+jest.mock("@/engine/scripts/register/schemes_registrator", () => ({ registerSchemes: jest.fn() }));
+
+import { updateClassIds } from "@/engine/core/utils/class_ids";
+import { unlockSystemIniOverriding } from "@/engine/core/utils/ini";
 import { AnyObject } from "@/engine/lib/types";
+import { registerExtensions } from "@/engine/scripts/register/extensions_registrator";
+import { registerManagers } from "@/engine/scripts/register/managers_registrator";
+import { registerSchemes } from "@/engine/scripts/register/schemes_registrator";
 
 describe("'start' entry point", () => {
   const checkBinding = (name: string, container: AnyObject = global) => {
@@ -13,5 +24,17 @@ describe("'start' entry point", () => {
     require("@/engine/scripts/start");
 
     checkBinding("callback");
+  });
+
+  it("should correctly init methods on start", () => {
+    require("@/engine/scripts/start");
+
+    (global as AnyObject).start.callback();
+
+    expect(updateClassIds).toHaveBeenCalledTimes(1);
+    expect(unlockSystemIniOverriding).toHaveBeenCalledTimes(1);
+    expect(registerExtensions).toHaveBeenCalledTimes(1);
+    expect(registerManagers).toHaveBeenCalledTimes(1);
+    expect(registerSchemes).toHaveBeenCalledTimes(1);
   });
 });
