c8b8d4eddb532881b58b66b7bb08bc327d9856d6
Neloreck <Neloreck@gmail.com>
2023-10-20 01:38:37 +0300

    Adding some game callbacks tests.

--- src/engine/scripts/declarations/callbacks/game.test.ts
+++ src/engine/scripts/declarations/callbacks/game.test.ts
@@ -1,6 +1,8 @@
-import { beforeAll, describe, it } from "@jest/globals";
+import { beforeAll, describe, expect, it } from "@jest/globals";
 
-import { checkBinding, checkNestedBinding } from "@/fixtures/engine";
+import { smartCoversList } from "@/engine/core/objects/animation/smart_covers";
+import { AnyObject } from "@/engine/lib/types";
+import { callBinding, checkBinding, checkNestedBinding } from "@/fixtures/engine";
 
 describe("game external callbacks", () => {
   beforeAll(() => {
@@ -14,4 +16,24 @@ describe("game external callbacks", () => {
     checkBinding("outro");
     checkBinding("trade_manager");
   });
+
+  it("main to be defined for custom scripts", () => {
+    expect(() => callBinding("main")).not.toThrow();
+  });
+
+  it("smart_covers should be defined", () => {
+    expect((_G as AnyObject).smart_covers).toBe(smartCoversList);
+  });
+
+  it.todo("outro callbacks should be defined");
+
+  it.todo("trade manager callbacks should be defined");
+
+  it.todo("on_before_game_save should be handled");
+
+  it.todo("on_game_save should be handled");
+
+  it.todo("on_before_game_load should be handled");
+
+  it.todo("on_game_load should be handled");
 });
