3b452b41a657917ccf6bb231ff34a6f26a46fe2a
Neloreck <Neloreck@gmail.com>
2023-10-20 23:12:53 +0300

    Game callbacks tests.

--- src/engine/scripts/declarations/callbacks/game.test.ts
+++ src/engine/scripts/declarations/callbacks/game.test.ts
@@ -1,12 +1,19 @@
-import { beforeAll, describe, expect, it } from "@jest/globals";
+import { beforeAll, beforeEach, describe, expect, it, jest } from "@jest/globals";
 
+import { SaveManager } from "@/engine/core/managers/save";
+import { TradeManager } from "@/engine/core/managers/trade";
 import { smartCoversList } from "@/engine/core/objects/animation/smart_covers";
-import { AnyObject } from "@/engine/lib/types";
-import { callBinding, checkBinding, checkNestedBinding } from "@/fixtures/engine";
+import { GameOutroManager } from "@/engine/core/ui/game/GameOutroManager";
+import { AnyArgs, AnyObject, TName } from "@/engine/lib/types";
+import { callBinding, checkBinding, checkNestedBinding, resetRegistry } from "@/fixtures/engine";
 
 describe("game external callbacks", () => {
   beforeAll(() => {
     require("@/engine/scripts/declarations/callbacks/game");
+  });
+
+  beforeEach(() => {
+    resetRegistry();
   });
 
   it("should correctly inject external methods for game", () => {
@@ -22,18 +29,84 @@ describe("game external callbacks", () => {
   });
 
   it("smart_covers should be defined", () => {
-    expect((_G as AnyObject).smart_covers).toBe(smartCoversList);
+    expect((_G as AnyObject).smart_covers.descriptions).toBe(smartCoversList);
   });
 
-  it.todo("outro callbacks should be defined");
+  it("outro callbacks should be defined", () => {
+    const outroManager: GameOutroManager = GameOutroManager.getInstance();
 
-  it.todo("trade manager callbacks should be defined");
+    jest.spyOn(outroManager, "startBkSound").mockImplementation(jest.fn());
+    jest.spyOn(outroManager, "stopBkSound").mockImplementation(jest.fn());
+    jest.spyOn(outroManager, "updateBkSoundFadeStart").mockImplementation(jest.fn());
+    jest.spyOn(outroManager, "updateBkSoundFadeStop").mockImplementation(jest.fn());
 
-  it.todo("on_before_game_save should be handled");
+    const callOutroBinding = (name: TName, args: AnyArgs = []) => callBinding(name, args, (_G as AnyObject)["outro"]);
 
-  it.todo("on_game_save should be handled");
+    expect((_G as AnyObject)["outro"]["conditions"]).toBe(GameOutroManager.OUTRO_CONDITIONS);
 
-  it.todo("on_before_game_load should be handled");
+    callOutroBinding("start_bk_sound");
+    expect(outroManager.startBkSound).toHaveBeenCalled();
 
-  it.todo("on_game_load should be handled");
+    callOutroBinding("stop_bk_sound");
+    expect(outroManager.stopBkSound).toHaveBeenCalled();
+
+    callOutroBinding("update_bk_sound_fade_start", [25]);
+    expect(outroManager.updateBkSoundFadeStart).toHaveBeenCalledWith(25);
+    expect(outroManager.stopBkSound).toHaveBeenCalled();
+
+    callOutroBinding("update_bk_sound_fade_stop", [35]);
+    expect(outroManager.updateBkSoundFadeStop).toHaveBeenCalledWith(35);
+  });
+
+  it("trade manager callbacks should be defined", () => {
+    const tradeManager: TradeManager = TradeManager.getInstance();
+
+    const callTradeBinding = (name: TName, args: AnyArgs = []) =>
+      callBinding(name, args, (_G as AnyObject)["trade_manager"]);
+
+    jest.spyOn(tradeManager, "getSellDiscountForObject").mockImplementation(() => 10);
+    jest.spyOn(tradeManager, "getBuyDiscountForObject").mockImplementation(() => 20);
+
+    expect(callTradeBinding("get_sell_discount", [30])).toBe(10);
+    expect(tradeManager.getSellDiscountForObject).toHaveBeenCalledWith(30);
+
+    expect(callTradeBinding("get_buy_discount", [40])).toBe(20);
+    expect(tradeManager.getBuyDiscountForObject).toHaveBeenCalledWith(40);
+  });
+
+  it("on_before_game_save should be handled", () => {
+    const saveManager: SaveManager = SaveManager.getInstance();
+
+    jest.spyOn(saveManager, "onBeforeGameSave");
+
+    callBinding("on_before_game_save", ["name"]);
+    expect(saveManager.onBeforeGameSave).toHaveBeenCalledWith("name");
+  });
+
+  it("on_game_save should be handled", () => {
+    const saveManager: SaveManager = SaveManager.getInstance();
+
+    jest.spyOn(saveManager, "onGameSave");
+
+    callBinding("on_game_save", ["name"]);
+    expect(saveManager.onGameSave).toHaveBeenCalledWith("name");
+  });
+
+  it("on_before_game_load should be handled", () => {
+    const saveManager: SaveManager = SaveManager.getInstance();
+
+    jest.spyOn(saveManager, "onBeforeGameLoad");
+
+    callBinding("on_before_game_load", ["name"]);
+    expect(saveManager.onBeforeGameLoad).toHaveBeenCalledWith("name");
+  });
+
+  it("on_game_load should be handled", () => {
+    const saveManager: SaveManager = SaveManager.getInstance();
+
+    jest.spyOn(saveManager, "onGameLoad");
+
+    callBinding("on_game_load", ["name"]);
+    expect(saveManager.onGameLoad).toHaveBeenCalledWith("name");
+  });
 });
