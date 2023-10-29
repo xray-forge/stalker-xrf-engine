b0bb5047b793be66116343e9886cb139156f9a39
Neloreck <Neloreck@gmail.com>
2023-10-29 15:04:24 +0200

    Add test instead of placeholder for abuse evaluator.

--- src/engine/core/schemes/stalker/abuse/evaluators/EvaluatorAbuse.test.ts
+++ src/engine/core/schemes/stalker/abuse/evaluators/EvaluatorAbuse.test.ts
@@ -1,5 +1,23 @@
-import { describe, it } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
+
+import { AbuseManager, ISchemeAbuseState } from "@/engine/core/schemes/stalker/abuse";
+import { EvaluatorAbuse } from "@/engine/core/schemes/stalker/abuse/evaluators/EvaluatorAbuse";
+import { EScheme } from "@/engine/lib/types";
+import { mockSchemeState } from "@/fixtures/engine";
 
 describe("EvaluatorAbuse class", () => {
-  it.todo("should correctly check if abuse is necessary");
+  it("should correctly check if abuse is necessary", () => {
+    const state: ISchemeAbuseState = mockSchemeState(EScheme.ABUSE);
+    const evaluator: EvaluatorAbuse = new EvaluatorAbuse(state);
+
+    state.abuseManager = { update: jest.fn(() => true) } as unknown as AbuseManager;
+
+    jest.spyOn(state.abuseManager, "update").mockImplementation(() => false);
+    expect(evaluator.evaluate()).toBe(false);
+    expect(state.abuseManager.update).toHaveBeenCalledTimes(1);
+
+    jest.spyOn(state.abuseManager, "update").mockImplementation(() => true);
+    expect(evaluator.evaluate()).toBe(true);
+    expect(state.abuseManager.update).toHaveBeenCalledTimes(2);
+  });
 });
