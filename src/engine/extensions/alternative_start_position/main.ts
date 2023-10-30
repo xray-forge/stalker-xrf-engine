619f518535bd42d1d569dee9b87b188033edd8da
Neloreck <Neloreck@gmail.com>
2023-10-30 03:00:10 +0200

    Apply start position only if current level is zaton.

--- src/engine/extensions/alternative_start_position/main.ts
+++ src/engine/extensions/alternative_start_position/main.ts
@@ -1,7 +1,8 @@
-import { set_start_game_vertex_id, set_start_position } from "xray16";
+import { level, set_start_game_vertex_id, set_start_position } from "xray16";
 
 import { LuaLogger } from "@/engine/core/utils/logging";
 import { createVector } from "@/engine/core/utils/vector";
+import { levels } from "@/engine/lib/constants/levels";
 import { TName } from "@/engine/lib/types";
 
 const logger: LuaLogger = new LuaLogger($filename);
@@ -12,7 +13,7 @@ export const enabled: boolean = true;
 export function register(isNewGame: boolean): void {
   logger.info("Alternative start extension register:", isNewGame);
 
-  if (isNewGame) {
+  if (isNewGame && level.name() === levels.zaton) {
     set_start_game_vertex_id(212);
     set_start_position(createVector(-307.5, 16.5, 550.5));
   }
