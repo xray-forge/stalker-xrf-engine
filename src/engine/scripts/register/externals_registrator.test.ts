df08f59081732f0cec078b545659305a7b1e9a07
Neloreck <Neloreck@gmail.com>
2024-01-15 21:18:26 +0200

    Check re-declarations with register externals method.

--- src/engine/scripts/register/externals_registrator.test.ts
+++ src/engine/scripts/register/externals_registrator.test.ts
@@ -1,11 +1,13 @@
 import { describe, expect, it } from "@jest/globals";
 
 import { getExtern } from "@/engine/core/utils/binding";
+import { AnyCallable } from "@/engine/lib/types";
 import { registerExternals } from "@/engine/scripts/register/externals_registrator";
 
 describe("extrnals_registrator entry point", () => {
   it("registerExternals should correctly register globals and mark as called", () => {
     expect(getExtern("areExternalsRegistered")).toBeFalsy();
+
     registerExternals();
 
     expect(getExtern("areExternalsRegistered")).toBeTruthy();
@@ -21,5 +23,14 @@ describe("extrnals_registrator entry point", () => {
     expect(getExtern("dialogs_pripyat")).toBeDefined();
     expect(getExtern("dialogs_jupiter")).toBeDefined();
     expect(getExtern("dialogs_zaton")).toBeDefined();
+
+    // Do not re-declare same values.
+    const previous: AnyCallable = getExtern("on_actor_critical_power");
+
+    expect(() => {
+      registerExternals();
+    }).not.toThrow();
+
+    expect(previous).toBe("on_actor_critical_power");
   });
 });
