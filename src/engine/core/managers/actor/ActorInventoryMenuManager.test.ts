d1f3d9ac7b6ea58aa3a576d6a9fcead894617104
Neloreck <Neloreck@gmail.com>
2024-02-04 21:42:03 +0200

    ActorInventoryMenuManager tests.

--- src/engine/core/managers/actor/ActorInventoryMenuManager.test.ts
+++ src/engine/core/managers/actor/ActorInventoryMenuManager.test.ts
@@ -3,9 +3,9 @@ import { beforeEach, describe, expect, it, jest } from "@jest/globals";
 import { getManager } from "@/engine/core/database";
 import { actorConfig } from "@/engine/core/managers/actor/ActorConfig";
 import { ActorInventoryMenuManager } from "@/engine/core/managers/actor/ActorInventoryMenuManager";
-import { AnyCallable, Console, EActorMenuMode } from "@/engine/lib/types";
+import { AnyCallable, Console, EActorMenuMode, EActorMenuType } from "@/engine/lib/types";
 import { resetRegistry } from "@/fixtures/engine";
-import { MockConsole } from "@/fixtures/xray";
+import { MockConsole, MockGameObject } from "@/fixtures/xray";
 
 describe("ActorInventoryMenuManager", () => {
   beforeEach(() => {
@@ -18,7 +18,7 @@ describe("ActorInventoryMenuManager", () => {
 
   it("should correctly initialize", () => {
     const console: Console = MockConsole.getInstanceMock();
-    const actorInventoryMenuManager: ActorInventoryMenuManager = getManager(ActorInventoryMenuManager);
+    const manager: ActorInventoryMenuManager = getManager(ActorInventoryMenuManager);
 
     expect(actorConfig.ACTOR_MENU_MODE).toBe(EActorMenuMode.UNDEFINED);
 
@@ -32,31 +32,84 @@ describe("ActorInventoryMenuManager", () => {
     ]);
   });
 
-  it("should correctly change and check active mode", () => {
-    const actorInventoryMenuManager: ActorInventoryMenuManager = getManager(ActorInventoryMenuManager);
+  it("should correctly set and check active mode", () => {
+    const manager: ActorInventoryMenuManager = getManager(ActorInventoryMenuManager);
 
     expect(actorConfig.ACTOR_MENU_MODE).toBe(EActorMenuMode.UNDEFINED);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(true);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.TALK_DIALOG)).toBe(false);
+    expect(manager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(true);
+    expect(manager.isActiveMode(EActorMenuMode.TALK_DIALOG)).toBe(false);
 
-    actorInventoryMenuManager.setActiveMode(EActorMenuMode.INVENTORY);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(false);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.INVENTORY)).toBe(true);
+    manager.setActiveMode(EActorMenuMode.INVENTORY);
+    expect(manager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(false);
+    expect(manager.isActiveMode(EActorMenuMode.INVENTORY)).toBe(true);
 
-    actorInventoryMenuManager.setActiveMode(EActorMenuMode.UNDEFINED);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(true);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.INVENTORY)).toBe(false);
+    manager.setActiveMode(EActorMenuMode.UNDEFINED);
+    expect(manager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(true);
+    expect(manager.isActiveMode(EActorMenuMode.INVENTORY)).toBe(false);
 
-    actorInventoryMenuManager.setActiveMode(EActorMenuMode.TALK_DIALOG_SHOW);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(false);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.TALK_DIALOG)).toBe(true);
+    manager.setActiveMode(EActorMenuMode.TALK_DIALOG_SHOW);
+    expect(manager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(false);
+    expect(manager.isActiveMode(EActorMenuMode.TALK_DIALOG)).toBe(true);
 
-    actorInventoryMenuManager.setActiveMode(EActorMenuMode.TALK_DIALOG_HIDE);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(true);
-    expect(actorInventoryMenuManager.isActiveMode(EActorMenuMode.TALK_DIALOG)).toBe(false);
+    manager.setActiveMode(EActorMenuMode.TALK_DIALOG_HIDE);
+    expect(manager.isActiveMode(EActorMenuMode.UNDEFINED)).toBe(true);
+    expect(manager.isActiveMode(EActorMenuMode.TALK_DIALOG)).toBe(false);
   });
 
-  it.todo("should correctly handle open actor menu event");
+  it("should correctly open actor menu", () => {
+    const manager: ActorInventoryMenuManager = getManager(ActorInventoryMenuManager);
 
-  it.todo("should correctly handle close actor menu event");
+    jest.spyOn(manager, "onWindowOpen").mockImplementation(jest.fn());
+
+    manager.openActorMenu(EActorMenuMode.INVENTORY);
+
+    expect(actorConfig.ACTOR_MENU_MODE).toBe(EActorMenuMode.INVENTORY);
+    expect(manager.onWindowOpen).toHaveBeenCalledWith(EActorMenuMode.INVENTORY);
+
+    manager.openActorMenu(EActorMenuMode.TALK_DIALOG_SHOW);
+
+    expect(actorConfig.ACTOR_MENU_MODE).toBe(EActorMenuMode.TALK_DIALOG);
+    expect(manager.onWindowOpen).toHaveBeenCalledWith(EActorMenuMode.TALK_DIALOG_SHOW);
+  });
+
+  it("should correctly close actor menu", () => {
+    const manager: ActorInventoryMenuManager = getManager(ActorInventoryMenuManager);
+
+    jest.spyOn(manager, "onWindowClosed").mockImplementation(jest.fn());
+
+    actorConfig.ACTOR_MENU_MODE = EActorMenuMode.DEAD_BODY_SEARCH;
+    manager.closeActorMenu();
+
+    expect(actorConfig.ACTOR_MENU_MODE).toBe(EActorMenuMode.UNDEFINED);
+    expect(manager.onWindowClosed).toHaveBeenCalledWith(EActorMenuMode.DEAD_BODY_SEARCH);
+
+    actorConfig.ACTOR_MENU_MODE = EActorMenuMode.TALK_DIALOG_HIDE;
+    manager.closeActorMenu();
+
+    expect(actorConfig.ACTOR_MENU_MODE).toBe(EActorMenuMode.UNDEFINED);
+    expect(manager.onWindowClosed).toHaveBeenCalledWith(EActorMenuMode.TALK_DIALOG);
+  });
+
+  it("should correctly handle drop item event", () => {
+    expect(() => {
+      getManager(ActorInventoryMenuManager).onItemDropped(
+        MockGameObject.mock(),
+        MockGameObject.mock(),
+        EActorMenuType.ACTOR_BAG,
+        EActorMenuType.ACTOR_BELT
+      );
+    }).not.toThrow();
+  });
+
+  it("should correctly handle open actor menu event", () => {
+    expect(() => {
+      getManager(ActorInventoryMenuManager).onWindowOpen(EActorMenuMode.TALK_DIALOG);
+    }).not.toThrow();
+  });
+
+  it("should correctly handle close actor menu event", () => {
+    expect(() => {
+      getManager(ActorInventoryMenuManager).onWindowClosed(EActorMenuMode.TALK_DIALOG);
+    }).not.toThrow();
+  });
 });
--- src/engine/core/managers/actor/ActorInventoryMenuManager.ts
+++ src/engine/core/managers/actor/ActorInventoryMenuManager.ts
@@ -10,19 +10,29 @@ import { EActorMenuMode, EActorMenuType, GameObject, IniFile } from "@/engine/li
 
 const logger: LuaLogger = new LuaLogger($filename);
 
-// todo: .CUIActorMenu_OnItemDropped handler
+/**
+ * Manager to synchronize and handle actor inventory events.
+ */
 export class ActorInventoryMenuManager extends AbstractManager {
   public override initialize(): void {
     const ini: IniFile = SYSTEM_INI;
 
-    executeConsoleCommand(consoleCommands.slot_0, readIniString(ini, ACTOR, "quick_item_1", false, null, ""));
-    executeConsoleCommand(consoleCommands.slot_1, readIniString(ini, ACTOR, "quick_item_2", false, null, ""));
-    executeConsoleCommand(consoleCommands.slot_2, readIniString(ini, ACTOR, "quick_item_3", false, null, ""));
-    executeConsoleCommand(consoleCommands.slot_3, readIniString(ini, ACTOR, "quick_item_4", false, null, ""));
+    executeConsoleCommand(consoleCommands.slot_0, readIniString(ini, ACTOR, "quick_item_1") ?? "");
+    executeConsoleCommand(consoleCommands.slot_1, readIniString(ini, ACTOR, "quick_item_2") ?? "");
+    executeConsoleCommand(consoleCommands.slot_2, readIniString(ini, ACTOR, "quick_item_3") ?? "");
+    executeConsoleCommand(consoleCommands.slot_3, readIniString(ini, ACTOR, "quick_item_4") ?? "");
   }
 
   /**
-   * todo: Description.
+   * @param mode - type of inventory to check
+   * @returns whether current inventory mode matches provided type
+   */
+  public isActiveMode(mode: EActorMenuMode): boolean {
+    return actorConfig.ACTOR_MENU_MODE === mode;
+  }
+
+  /**
+   * @param nextMode - mode to set in inventory
    */
   public setActiveMode(nextMode: EActorMenuMode): void {
     if (nextMode === EActorMenuMode.UNDEFINED || nextMode === EActorMenuMode.TALK_DIALOG_HIDE) {
@@ -33,27 +43,9 @@ export class ActorInventoryMenuManager extends AbstractManager {
   }
 
   /**
-   * todo: Description.
-   */
-  public closeActorMenu(): void {
-    switch (actorConfig.ACTOR_MENU_MODE) {
-      case EActorMenuMode.INVENTORY:
-      case EActorMenuMode.TRADE:
-      case EActorMenuMode.UPGRADE:
-      case EActorMenuMode.DEAD_BODY_SEARCH:
-        this.onWindowClosed(actorConfig.ACTOR_MENU_MODE);
-        break;
-
-      case EActorMenuMode.TALK_DIALOG_HIDE:
-        this.onWindowClosed(EActorMenuMode.TALK_DIALOG);
-        break;
-    }
-
-    actorConfig.ACTOR_MENU_MODE = EActorMenuMode.UNDEFINED;
-  }
-
-  /**
-   * todo: Description.
+   * Open in-game hud menu.
+   *
+   * @param mode - menu mode to show
    */
   public openActorMenu(mode: EActorMenuMode): void {
     switch (mode) {
@@ -73,28 +65,44 @@ export class ActorInventoryMenuManager extends AbstractManager {
   }
 
   /**
-   * todo: Description.
+   * Close currently active in-game menu.
    */
-  public isActiveMode(mode: EActorMenuMode): boolean {
-    return actorConfig.ACTOR_MENU_MODE === mode;
+  public closeActorMenu(): void {
+    switch (actorConfig.ACTOR_MENU_MODE) {
+      case EActorMenuMode.INVENTORY:
+      case EActorMenuMode.TRADE:
+      case EActorMenuMode.UPGRADE:
+      case EActorMenuMode.DEAD_BODY_SEARCH:
+        this.onWindowClosed(actorConfig.ACTOR_MENU_MODE);
+        break;
+
+      case EActorMenuMode.TALK_DIALOG_HIDE:
+        this.onWindowClosed(EActorMenuMode.TALK_DIALOG);
+        break;
+    }
+
+    actorConfig.ACTOR_MENU_MODE = EActorMenuMode.UNDEFINED;
   }
 
   /**
-   * todo: Description.
+   * @param from - game object owner of previous list
+   * @param to - game object owner of next list
+   * @param oldList - type of previous list
+   * @param newList - type of next list
    */
   public onItemDropped(from: GameObject, to: GameObject, oldList: EActorMenuType, newList: EActorMenuType): void {
     logger.info("Actor menu inventory item dropped: %s %s %s %s", from?.name(), to?.name(), oldList, newList);
   }
 
   /**
-   * todo: Description.
+   * @param mode - type of actor menu open
    */
   public onWindowOpen(mode: EActorMenuMode): void {
     logger.info("Actor menu open: %s", EActorMenuMode[mode]);
   }
 
   /**
-   * todo: Description.
+   * @param mode - type of actor menu closed
    */
   public onWindowClosed(mode: EActorMenuMode): void {
     logger.info("Actor menu close: %s", EActorMenuMode[mode]);
