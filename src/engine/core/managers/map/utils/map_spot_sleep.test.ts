099d657b9298116d88df54bc3888e62452321feb
Neloreck <Neloreck@gmail.com>
2024-05-05 01:26:27 +0300

    Add test for sleep zones display. Minimal update of the method.
    Signed-off-by: Neloreck <Neloreck@gmail.com>

--- src/engine/core/managers/map/utils/map_spot_sleep.test.ts
+++ src/engine/core/managers/map/utils/map_spot_sleep.test.ts
@@ -1,5 +1,61 @@
-import { describe, it } from "@jest/globals";
+import { beforeEach, describe, expect, it, jest } from "@jest/globals";
+import { level } from "xray16";
+
+import { registerObject, registerStoryLink } from "@/engine/core/database";
+import { updateSleepZonesDisplay } from "@/engine/core/managers/map/utils/map_spot_sleep";
+import { GameObject } from "@/engine/lib/types";
+import { mockRegisteredActor, resetRegistry } from "@/fixtures/engine";
+import { resetFunctionMock } from "@/fixtures/jest";
+import { MockGameObject } from "@/fixtures/xray";
 
 describe("updateSleepZonesDisplay util", () => {
-  it.todo("should correctly update display for in-game sleep zones");
+  beforeEach(() => {
+    resetRegistry();
+    resetFunctionMock(level.map_add_object_spot);
+    resetFunctionMock(level.map_remove_object_spot);
+  });
+
+  it("should correctly update display for in-game sleep zones when near", () => {
+    const { actorGameObject } = mockRegisteredActor();
+    const first: GameObject = MockGameObject.mock();
+    const second: GameObject = MockGameObject.mock();
+
+    registerStoryLink(first.id(), "zat_a2_sr_sleep_id");
+    registerStoryLink(second.id(), "jup_a6_sr_sleep_id");
+
+    registerObject(first);
+    registerObject(second);
+
+    jest.spyOn(level, "map_has_object_spot").mockImplementation(() => 0);
+    jest.spyOn(actorGameObject.position(), "distance_to").mockImplementation(() => 74);
+
+    updateSleepZonesDisplay();
+
+    expect(level.map_remove_object_spot).toHaveBeenCalledTimes(0);
+    expect(level.map_add_object_spot).toHaveBeenCalledTimes(2);
+    expect(level.map_add_object_spot).toHaveBeenCalledWith(
+      first.id(),
+      "ui_pda2_actor_sleep_location",
+      "st_ui_pda_sleep_place"
+    );
+    expect(level.map_add_object_spot).toHaveBeenCalledWith(
+      second.id(),
+      "ui_pda2_actor_sleep_location",
+      "st_ui_pda_sleep_place"
+    );
+
+    jest.spyOn(level, "map_has_object_spot").mockImplementation(() => 1);
+
+    updateSleepZonesDisplay();
+
+    expect(level.map_remove_object_spot).toHaveBeenCalledTimes(0);
+    expect(level.map_add_object_spot).toHaveBeenCalledTimes(2);
+
+    jest.spyOn(actorGameObject.position(), "distance_to").mockImplementation(() => 76);
+
+    updateSleepZonesDisplay();
+
+    expect(level.map_remove_object_spot).toHaveBeenCalledTimes(2);
+    expect(level.map_add_object_spot).toHaveBeenCalledTimes(2);
+  });
 });
--- src/engine/core/managers/map/utils/map_spot_sleep.ts
+++ src/engine/core/managers/map/utils/map_spot_sleep.ts
@@ -6,20 +6,24 @@ import { mapMarks } from "@/engine/lib/constants/map_marks";
 import { GameObject, Optional, TDistance, TNumberId, Vector } from "@/engine/lib/types";
 
 /**
- * todo: Description.
+ * Update display of sleep zones.
+ * Based on distance to zone display it on map or hide.
+ *
+ * By default, there are 3 sleep zones in stalker COP.
  */
 export function updateSleepZonesDisplay(): void {
-  for (const [, sleepZone] of mapDisplayConfig.SLEEP_SPOTS) {
-    const objectId: Optional<TNumberId> = getObjectIdByStoryId(sleepZone.target);
+  const actorPosition: Vector = registry.actor.position();
+
+  for (const [, spot] of mapDisplayConfig.SLEEP_SPOTS) {
+    const objectId: Optional<TNumberId> = getObjectIdByStoryId(spot.target);
     const object: Optional<GameObject> = objectId ? registry.objects.get(objectId)?.object : null;
 
     if (objectId && object) {
-      const actorPosition: Vector = registry.actor.position();
-      const distanceFromActor: TDistance = object.position().distance_to(actorPosition);
+      const distanceFromActor: TDistance = actorPosition.distance_to(object.position());
       const hasSleepSpot: boolean = level.map_has_object_spot(objectId, mapMarks.ui_pda2_actor_sleep_location) !== 0;
 
       if (distanceFromActor <= mapDisplayConfig.DISTANCE_TO_DISPLAY && !hasSleepSpot) {
-        level.map_add_object_spot(objectId, mapMarks.ui_pda2_actor_sleep_location, sleepZone.hint);
+        level.map_add_object_spot(objectId, mapMarks.ui_pda2_actor_sleep_location, spot.hint);
       } else if (distanceFromActor > mapDisplayConfig.DISTANCE_TO_DISPLAY && hasSleepSpot) {
         level.map_remove_object_spot(objectId, mapMarks.ui_pda2_actor_sleep_location);
       }
