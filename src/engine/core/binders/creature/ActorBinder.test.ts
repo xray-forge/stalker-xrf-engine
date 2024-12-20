c48243d7f9225ddc6caa384e08760884fe334746
Neloreck <Neloreck@gmail.com>
2024-12-20 05:14:32 +0200

    Expect `hud_animation_end` declaration.
    Signed-off-by: Neloreck <Neloreck@gmail.com>

--- src/engine/core/binders/creature/ActorBinder.test.ts
+++ src/engine/core/binders/creature/ActorBinder.test.ts
@@ -121,12 +121,13 @@ describe("ActorBinder", () => {
 
     expect(actor.set_callback).toHaveBeenCalledTimes(8);
     expect(actor.set_callback).toHaveBeenCalledWith(callback.inventory_info, expect.any(Function));
+    expect(actor.set_callback).toHaveBeenCalledWith(callback.take_item_from_box, expect.any(Function));
     expect(actor.set_callback).toHaveBeenCalledWith(callback.on_item_take, expect.any(Function));
     expect(actor.set_callback).toHaveBeenCalledWith(callback.on_item_drop, expect.any(Function));
     expect(actor.set_callback).toHaveBeenCalledWith(callback.trade_sell_buy_item, expect.any(Function));
     expect(actor.set_callback).toHaveBeenCalledWith(callback.task_state, expect.any(Function));
-    expect(actor.set_callback).toHaveBeenCalledWith(callback.take_item_from_box, expect.any(Function));
     expect(actor.set_callback).toHaveBeenCalledWith(callback.use_object, expect.any(Function));
+    expect(actor.set_callback).toHaveBeenCalledWith(callback.hud_animation_end, expect.any(Function));
 
     expect(eventsManager.emitEvent).toHaveBeenCalledWith(EGameEvent.ACTOR_REINIT, binder);
   });
