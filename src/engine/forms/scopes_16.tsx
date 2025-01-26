44d592365ead1ac6328efb2cfddd316a21565d7e
Neloreck <Neloreck@gmail.com>
2025-01-26 17:15:39 +0200

    Add new crosshair.
    Signed-off-by: Neloreck <Neloreck@gmail.com>

--- src/engine/forms/scopes_16.tsx
+++ src/engine/forms/scopes_16.tsx
@@ -8,9 +8,11 @@ import { ScopeDescriptor } from "@/engine/forms/components/ScopeDescriptor.compo
 export function create(): JSXNode {
   return (
     <w>
-      {["wpn_crosshair", "wpn_crosshair_l85", "wpn_crosshair_g36", "wpn_crosshair_rpg"].map((it) => (
-        <ScopeDescriptor name={it} is169 />
-      ))}
+      {["wpn_crosshair", "wpn_crosshair_pso", "wpn_crosshair_l85", "wpn_crosshair_g36", "wpn_crosshair_rpg"].map(
+        (it) => (
+          <ScopeDescriptor name={it} is169 />
+        )
+      )}
 
       {["wpn_crosshair_bino"].map((it) => (
         <ScopeDescriptor name={it} />
