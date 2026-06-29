0178040ccb8547ce028c45622642112452c39b2f
Neloreck <Neloreck@gmail.com>
2026-06-29 17:56:39 +0300

    Do not do type assertions for jest `called` matchers.

--- src/typedefs/jest/index.d.ts
+++ src/typedefs/jest/index.d.ts
@@ -5,6 +5,9 @@ export * from "expect";
  */
 declare module "expect" {
   export interface Matchers<R extends void | Promise<void>> {
+    toHaveBeenCalledWith(...expected: Array<unknown>): R;
+    toHaveBeenLastCalledWith(...expected: Array<unknown>): R;
+    toHaveBeenNthCalledWith(nthCall: number, ...expected: Array<unknown>): R;
     toEqualLuaTables(expected: Record<string, any> | LuaTable | null): ExpectationResult;
     toStrictEqualLuaTables(expected: Record<string, any> | LuaTable | null): ExpectationResult;
     toEqualLuaArrays(expected: Array<unknown> | LuaTable<number, unknown> | null): ExpectationResult;
