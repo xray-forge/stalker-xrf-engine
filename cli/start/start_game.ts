94fb15c064b6622e706aba1750593ed4de4f4719
Neloreck <Neloreck@gmail.com>
2026-07-09 01:36:19 +0300

    Update `startGame` to use `spawn` for improved process control and fallback executable logic. Add `dump_bindings` parameter by default.

--- cli/start/start_game.ts
+++ cli/start/start_game.ts
@@ -1,11 +1,15 @@
-import { exec } from "node:child_process";
+import * as cp from "node:child_process";
+import * as path from "node:path";
 
 import { green, yellowBright } from "chalk";
 
+import { exists } from "#/utils/fs/exists";
 import { getGamePaths } from "#/utils/fs/get_game_paths";
 import { NodeLogger } from "#/utils/logging";
 
 const log: NodeLogger = NodeLogger.forFile(__filename);
+
+const START_GAME_ARGUMENTS: ReadonlyArray<string> = ["-dump_bindings"];
 
 /**
  * Start game executable provided in config.json file.
@@ -13,15 +17,25 @@ const log: NodeLogger = NodeLogger.forFile(__filename);
 export async function startGame(): Promise<void> {
   log.info("Starting game");
 
-  const { app } = await getGamePaths();
+  const { app, bin, root } = await getGamePaths();
 
-  log.info("Starting game app:", yellowBright(app));
+  const engineApp: string = path.join(bin, "xrEngine.exe");
+  const startApp: string = (await exists(engineApp)) ? engineApp : app;
 
-  exec(`"${app}"`, (error, data) => {
-    if (error !== null) {
-      log.error("Failed to start process:", error, data, "\n");
-    } else {
-      log.info("Started process:", green("OK"), "\n");
-    }
+  log.info("Starting game app:", yellowBright(startApp), START_GAME_ARGUMENTS.join(" "));
+
+  const game = cp.spawn(startApp, START_GAME_ARGUMENTS, {
+    cwd: root,
+    detached: true,
+    stdio: "ignore",
+  });
+
+  game.once("error", (error: Error) => {
+    log.error("Failed to start process:", error, "\n");
+  });
+
+  game.once("spawn", () => {
+    game.unref();
+    log.info("Started process:", green("OK"), "\n");
   });
 }
